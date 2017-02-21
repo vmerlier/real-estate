bd2bafce16b002d352878e81a2da66fd6574a8a2
vmerlier <victorien.merlier@gmail.com>
2017-02-21 22:21:33 +0100

    TP3

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require( 'express' );
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
 
+data = {
+    ville: "",
+    price: 0,
+    codePostal: 0,
+    nbPieces: 0,
+    surface: 0
+}
 
 //creating a new express server
 var app = express();
@@ -41,28 +48,33 @@ function TellMe( Url, res ) {
         if ( !error && response.statusCode == 200 ) {
             var $ = cheerio.load( html );
 
-            var price = parseInt( $( 'h2.item_price span.value' ).get()[0].children[0].data.replace( /\s/g, '' ), 10 );
 
-            var ville = $( 'h2.clearfix span.value' ).get()[1].children[0].data.split( " " )[0];
 
-            var codePostal = $( 'h2.clearfix span.value' ).get()[1].children[0].data.split( " " )[1].replace( /\n/g, '' );
+            data.price = parseInt( $( 'h2.item_price span.value' ).get()[0].children[0].data.replace( /\s/g, '' ), 10 ),
 
-            var nbPieces = $( 'h2.clearfix span.value' ).get()[3].children[0].data;
+                data.ville = $( 'h2.clearfix span.value' ).get()[1].children[0].data.split( " " )[0],
 
-            var surface = parseInt( $( 'h2.clearfix span.value' ).get()[5].children[0].data.split( " " )[0], 10 );
+                data.codePostal = $( 'h2.clearfix span.value' ).get()[1].children[0].data.split( " " )[1].replace( /\n/g, '' ),
 
-            console.log( price, ville, codePostal, nbPieces, surface );
+                data.nbPieces = $( 'h2.clearfix span.value' ).get()[3].children[0].data,
 
+                data.surface = parseInt( $( 'h2.clearfix span.value' ).get()[5].children[0].data.split( " " )[0], 10 )
+
+
+            console.log( data.price, data.ville, data.codePostal, data.nbPieces, data.surface );
+
+        }
+        else {
+            res.render( 'home', {
+                message: "URL Invalide",
+            });
         }
 
 
 
-        search = "https://www.meilleursagents.com/prix-immobilier/" + ( ville.toLowerCase() + "-" + codePostal + "/" ).toString();
+        search = "https://www.meilleursagents.com/prix-immobilier/" + ( ( data.ville ).toLowerCase() + "-" + data.codePostal + "/" ).toString();
 
         request( search.toString(), function ( error, response, html ) {
-
-
-            console.log( response.statusCode );
 
 
             if ( !error && response.statusCode == 200 ) {
@@ -70,13 +82,14 @@ function TellMe( Url, res ) {
 
                 var meanPrice = parseInt( $( 'div.prices-summary__values div.prices-summary__cell--median' ).get( 1 ).children[0].data.replace( /\s/g, '' ), 10 );
 
-                if ( ( price / surface ) >= meanPrice ) {
-
+                if ( ( data.price / data.surface ) >= meanPrice ) {
+                    console.log( "Mauvais deal" );
                     res.render( 'home', {
                         message: "Mauvais deal",
                     });
                 }
                 else {
+                    console.log( "Bon deal" );
                     res.render( 'home', {
                         message: "Bon deal",
                     });
